a1ce167cba16f7f4ef8a61b344fb8a2309f09099
gaeqs <gael.rial.costas@gmail.com>
2026-01-21 00:39:14 +0100

    fix: Bump version 0.0.2

--- Examples/build.gradle.kts
+++ Examples/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.degoos"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 val hytaleInstallationPath: String by extra
 val hytaleServerExecutablePath: String by extra
